5782a59e56342e4af44897469ea76af117a4a07f
fdiengott <freddiengott@gmail.com>
2021-04-28 15:33:05 -0400

    stub out a tile#handleClick function

--- minesweeper/components/tile.jsx
+++ minesweeper/components/tile.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 
-export const Tile = ({tile}) => {
-    const count = tile.adjacentBombCount();
-
-    let content;
-
-    if (tile.explored && count > 0) {
-        content = <div className="tile explored">{count}</div>
-    } else if (tile.flagged) {
-        content = <div className="tile flagged">&#x2691;</div>
-    } else {
-        content = <div className="tile unexplored"></div>
+export default class Tile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    return content; 
+    handleClick(e) {
+        e.preventDefault; 
+        return <div onClick={this.props.updateGame}></div>
+    }
+
+    render() {   
+        const tile = this.props.tile; 
+        const count = tile.adjacentBombCount();
+        let content;
+        
+        if (tile.explored && count > 0) {
+            content = <div className="tile explored">{count}</div>
+        } else if (tile.flagged) {
+            content = <div className="tile flagged">&#x2691;</div>
+        } else {
+            content = <div className="tile unexplored"></div>
+        }
+        
+        return content; 
+    }
 };
